5d3ee3c267be802963d8c5d6e4906cc8765e3840
P3ND4 <106392394+P3ND4@users.noreply.github.com>
2025-10-01 14:14:17 -0500

    first commit

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: 'dashboard', loadComponent: () => import('./features/dashboard/dashboard').then(m => m.Dashboard), children:[
+        { path: 'products', loadComponent: () => import('./features/dashboard/products/products').then(m => m.Products) },
+    ] },
+    { path: '', redirectTo: 'dashboard/products', pathMatch: 'full' }
+];
