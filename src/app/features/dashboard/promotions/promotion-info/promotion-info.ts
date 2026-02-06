2249a05b1f57a94e604c0a8bfd36c1854d71d7cd
P3ND4 <josecarlospendasrodriguez@gmail.com>
2026-02-06 10:07:09 -0500

    promotions finished

--- src/app/features/dashboard/promotions/promotion-info/promotion-info.ts
+++ src/app/features/dashboard/promotions/promotion-info/promotion-info.ts
@@ -1,5 +1,6 @@
-import { Component, Input } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
 import { Promotion } from '../../../../shared/models/promotions';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-promotion-info',
@@ -8,9 +9,9 @@ import { Promotion } from '../../../../shared/models/promotions';
   styleUrl: './promotion-info.css'
 })
 export class PromotionInfo {
-  @Input({ required: true }) promotion!: Promotion
-
-
+  @Input({ required: true }) promotion!: Promotion;
+  @Output() close = new EventEmitter<void>();
+  constructor(private router: Router) { }
   convertToDate(date: Date) {
     date = new Date(date);
     let year = date.getFullYear().toString();
@@ -18,5 +19,13 @@ export class PromotionInfo {
     let day = (date.getDate() + 100).toString().slice(1);
 
     return year + '-' + month + '-' + day;
+  }
+
+  onClose() {
+    this.close.emit()
+  }
+
+  navigateToEdit() {
+    this.router.navigate([`/dashboard/create-promotion`], { queryParams: { edit: this.promotion.promo_id } });
   }
 }
